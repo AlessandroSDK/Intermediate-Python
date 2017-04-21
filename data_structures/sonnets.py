ae46d59a0723e47832dc494814e26831af0b0ab9
abresciani <abresciani@salesforce.com>
2017-04-21 16:33:27 +0100

    adding directory with data structures

---
+++ data_structures/sonnets.py
@@ -0,0 +1,67 @@
+#Slow version
+
+import time
+
+my_words = [elt.strip() for elt in open("sonnet_words.txt", "r").readlines()]
+word_list = [elt.strip() for elt in open("sowpods.txt", "r").readlines()]
+
+
+counter = 0
+
+start = time.time()
+
+for word in my_words:
+    if word not in word_list:
+        print(word)
+        counter += 1
+
+stop = time.time()
+
+print("Total new words: %d" % counter)
+print("Time elapsed: %.1f seconds" % (stop - start))
+
+
+#Improved faster version
+
+import time
+
+my_words = [elt.strip() for elt in open("sonnet_words.txt", "r").readlines()]
+word_list = [elt.strip() for elt in open("sowpods.txt", "r").readlines()]
+word_dict = dict((elt, 1) for elt in word_list)
+
+counter = 0
+
+start = time.time()
+
+for word in my_words:
+    if word not in word_dict:
+        print(word)
+        counter += 1
+
+stop = time.time()
+
+print("Total new words: %d" % counter)
+print("Time elapsed: %.1f seconds" % (stop - start))
+
+# Version with Sets
+
+import time
+
+my_words = [elt.strip() for elt in open("sonnet_words.txt", "r").readlines()]
+word_list = [elt.strip() for elt in open("sowpods.txt", "r").readlines()]
+word_set = set(word_list)
+
+counter = 0
+
+start = time.time()
+
+for word in my_words:
+    if word not in word_set:
+        print(word)
+        counter += 1
+
+stop = time.time()
+
+print("Total new words: %d" % counter)
+print("Time elapsed: %.1f seconds" % (stop - start))
+
